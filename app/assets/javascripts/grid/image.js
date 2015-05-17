4438df3ade84063065fcf60af33ea381e83620c2
Bernhard Boennemann <bmail@boenne.com>
2015-05-17 17:07:17 -0400

    grid votingfunctional but not styled

--- app/assets/javascripts/grid/image.js
+++ app/assets/javascripts/grid/image.js
@@ -1,7 +1,45 @@
+var index;
+var currentItemId;
+
 $(document).ready(function() {
+
 	$('.rate_star').click(function(){
-		var index = $(this).data('index');
-		$('#stars_' + index ).toggle('blind');
-		$('#result_' + index ).toggle('blind');
+		currentItemId = $(this).data('item');
+		index = $(this).data('index');
+		vote($(this).data('rating'));
 	});
 });
+
+
+// TODO: needs to be post method!
+function vote(value){
+	$.ajax({
+		url : '/grid_votings/' + votingId + '/image_items/' + currentItemId + '/vote_image_item.json?result=' + value,
+		type : 'get',
+		error : function(jqXHR, textStatus, errorThrown) {
+			alert('There was a problem loading the requested content. Please try again later');
+		},
+		success : function(item, resultText) {
+			
+			$('#stars_' + index ).toggle('blind');
+			populate_current_image(item, index);
+			$('#result_' + index ).toggle('blind');
+		}
+	});
+
+}
+
+
+function populate_current_image(item, index){	
+	$('#grid_image_score_' + index).text(item.score);
+	$('#grid_image_hits_' + index).text(item.hits);
+/*
+	if(previousItem.already_voted == true){
+		$('#already_voted').hide();
+		$('#already_voted').show( "bounce", { times: 3 }, "slow" );
+	}else{
+		$('#already_voted').hide();
+		$('#current_image').effect( "transfer", { to: $('#previous_image') }, 500 );
+	}
+	*/
+}
